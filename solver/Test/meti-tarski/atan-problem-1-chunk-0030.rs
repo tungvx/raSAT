bb86e58b2c961894df6beec7151ba5388871cf89
tungvx <toilatung90@gmail.com>
2014-05-26 17:05:41 +0900

    remove more unnecessary files

--- solver/Test/meti-tarski/atan-problem-1-chunk-0030.rs
+++
@@ -1,4 +0,0 @@
-(skoS3 in 0. 10.)
-(skoSX in 0. 10.)
-(skoX in 1. 10.)
-(assert (and (and (and (and (> (* 1. skoS3) 0.) (> (* 1. skoSX) 0.)) (> (* 1. skoX) 0.)) (> (* 1. skoX) 1.)) (> (+ (* -1. skoSX) (* -3. skoS3)) 0.)))
