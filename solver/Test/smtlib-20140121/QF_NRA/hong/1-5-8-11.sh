2c9f2ea26ba2d285b09351d60f32af5f546739b8
tung dep trai <toilatung90@gmail.com>
2015-05-22 18:07:39 +0900

    experiments files

---
+++ solver/Test/smtlib-20140121/QF_NRA/hong/1-5-8-11.sh
@@ -0,0 +1,4 @@
+directory=s1310007@hpcc:/work/tungvx/raSAT_2_1TCs_10_1DecomposedVar_1/QF_NRA/hong
+strategy=1-5-8-11
+date=20150522
+sshpass -p 'toilatung90!2' scp $directory/$strategy-$date.xls $strategy-$date.csv
---
+++ solver/Test/smtlib-20140121/QF_NRA/kissing/20150522/1-5-8-11.sh
@@ -0,0 +1,4 @@
+directory=s1310007@hpcc:/work/tungvx/raSAT_2_1TCs_10_1DecomposedVar_4/QF_NRA/kissing
+strategy=1-5-8-11
+date=20150522
+sshpass -p 'toilatung90!2' scp $directory/$strategy-$date.csv $strategy-$date.csv
