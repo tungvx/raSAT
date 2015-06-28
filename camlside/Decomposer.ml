cf25f3128b06ec14f386ebd786c60e2994872210
tungvx <toilatung90@gmail.com>
2015-06-28 13:34:46 +0900

    more changes

--- camlside/Decomposer.ml
+++ camlside/Decomposer.ml
@@ -1,182 +1,189 @@
-(*open Ast           (*type: literal, clause, formula... declaration*)
-open Util
-open Assignments
-open IA
-
-let decompose var interval decomposedPoint lstVarID iVar dIntv isLowerBound =
-  let id = List.assoc var lstVarID in
-  let si = "(or ("^ var ^ " in " ^ string_of_float interval#l ^ " " ^ string_of_float decomposedPoint ^ ") " ^
-		             "(" ^ var ^ " in " ^ string_of_float decomposedPoint ^ " " ^ string_of_float interval#h ^ "))" in
-  let sl = "-" ^ string_of_int id ^ " " ^ string_of_int (iVar + 1) ^ " " ^ string_of_int (iVar+2) ^ " 0 " ^
-		           string_of_int id ^ " -" ^ string_of_int (iVar + 1) ^ " 0 " ^
-		           "-" ^ string_of_int (iVar + 1) ^ " -"^string_of_int (iVar+2) ^ " 0 " ^
-		           string_of_int id ^ " -"^string_of_int (iVar+2) ^ " 0 " in		             
-  let bumpVarInt = 
-    if isLowerBound then iVar + 1
-    else iVar + 2
-  in
-  let bumpVarString = string_of_int bumpVarInt in
-  (*print_endline ("Bump vars: " ^ bumpVarString ^ " " ^ string_of_bool isLowerBound);
-  flush stdout;*)
-  if (dIntv <> "") then
-    ("(ic "^dIntv ^" "^si^")", sl, bumpVarString, true) 
-  else
-    (si, sl, bumpVarString, true)
-  
-
-let rec decompose_new_clause_extra (currentDIntv, currentMinisatCode, currentBumpVars, currentWasDecomposed) clause assIntv dIntv assignments lstVarID iVar esl varsList leftValue rightValue isSat isLowerBound =
-	match varsList with 
-	| [] -> (currentDIntv, currentMinisatCode, currentBumpVars, currentWasDecomposed)
-	| var::t -> 
-		(
-    let polyExp = get_exp clause in
-		let derivative = getDerivative polyExp var in
-		let derivativeValue = evalFloat assignments derivative in
-		let differentValue = leftValue -. rightValue in
-		if isLowerBound = (differentValue *. derivativeValue < 0.) then
-			let decomposedPoint = (List.assoc var assignments) -. differentValue /. finalDerivative in
-			let interval = List.assoc var assIntv in
-				if decomposedPoint -. interval#l > 0. && interval#h -. decomposedPoint > 0. then (
-					let id = List.assoc var lstVarID in
-					let si = "(or ("^ var ^ " in " ^ string_of_float interval#l ^ " " ^ string_of_float decomposedPoint ^ ") " ^
-		             "(" ^ var ^ " in " ^ string_of_float decomposedPoint ^ " " ^ string_of_float interval#h ^ "))" in
-
-		  		let sl = "-" ^ string_of_int id ^ " " ^ string_of_int (iVar + 1) ^ " " ^ string_of_int (iVar+2) ^ " 0 " ^
-		           string_of_int id ^ " -" ^ string_of_int (iVar + 1) ^ " 0 " ^
-		           "-" ^ string_of_int (iVar + 1) ^ " -"^string_of_int (iVar+2) ^ " 0 " ^
-		           string_of_int id ^ " -"^string_of_int (iVar+2) ^ " 0 " in
-          let bumpVarInt = 
-            if isLowerBound then 
-              if isSat then 
-                iVar + 2
-              else 
-                iVar + 1
-            else 
-              if isSat then 
-                iVar + 1
-              else      
-                iVar + 2        
-          in
-          let bumpVarString = string_of_int bumpVarInt in
-					let newResult =
-						if (currentDIntv <> "") then
-							if currentWasDecomposed then
-								("(ic "^currentDIntv ^" "^si^")", currentMinisatCode ^ sl, currentBumpVars ^ " " ^ bumpVarString, true)
-							else 
-								("(ic "^currentDIntv ^" "^si^")", sl, bumpVarString, true)
-						else
-							if currentWasDecomposed then
-								(si, currentMinisatCode ^ sl, currentBumpVars ^ " " ^ bumpVarString, true)
-							else 
-								(si, sl, bumpVarString, true)
-					in (*decompose_new_clause_extra newResult clause assIntv dIntv assignments lstVarID (iVar + 2) esl t leftValue rightValue isSat isLowerBound )*)
-            newResult )
-        else
-					decompose_new_clause_extra (currentDIntv, currentMinisatCode, currentBumpVars, currentWasDecomposed) clause assIntv dIntv assignments lstVarID iVar esl t leftValue rightValue isSat isLowerBound
-		else 
-			decompose_new_clause_extra (currentDIntv, currentMinisatCode, currentBumpVars, currentWasDecomposed) clause assIntv dIntv assignments lstVarID iVar esl t leftValue rightValue isSat isLowerBound
-		)
-
-  
-(* ====================== START dynamicDecom_new_clause ======================== *)
-(* | decompose a variable of an unknown clause based on Newtow's method *)
-(* | . clause: an unknown clause *)
-(* | . assIntv: the intervals of variables *)
-(* | . dIntv: the decomposed intervals from previous decompositions *)
-(* | . lstVarID: the list of maps from variables into their literal code in dIntv *)
-(* | . iVar: the current code of the last literal (in dIntv) *)
-(* | . esl: the minimum values of intervals for stopping decomposition *)
-let decompose_new_clause clause assIntv dIntv lstVarID iVar esl =
-  let lowerBound = getLowerBound assIntv [] in
-  let (lowerSat, lowerBoundLeftValue, lowerBoundRightValue) = checkSAT_computeValues clause lowerBound in
-  if lowerSat then 
-    let upperBound = getUpperBound assIntv [] in
-		let (upperSat, upperBoundLeftValue, upperBoundRightValue) = checkSAT_computeValues clause upperBound in
-		if upperSat then
-			("","","",false)
-		else 
-			let varsList = Util.red_list (bool_vars clause) in
-			decompose_new_clause_extra (dIntv, "", "", false) clause assIntv dIntv upperBound lstVarID iVar esl varsList upperBoundLeftValue upperBoundRightValue upperSat true
-	else
-		let varsList = Util.red_list (bool_vars clause) in
-		let (newDIntv, minisatCode, bumpVars, wasDecomposed) =
-		  decompose_new_clause_extra (dIntv, "", "", false) clause assIntv dIntv lowerBound lstVarID iVar esl varsList lowerBoundLeftValue lowerBoundRightValue lowerSat true
-		in 
-		  if wasDecomposed then 
-				(newDIntv, minisatCode, bumpVars, wasDecomposed)
-			else 
-				let upperBound = getUpperBound assIntv [] in
-				let (upperSat, upperBoundLeftValue, upperBoundRightValue) = checkSAT_computeValues clause upperBound in
-				if upperSat then
-					("","","",false)
-				else
-					let varsList = Util.red_list (bool_vars clause) in
-					decompose_new_clause_extra (dIntv, "", "", false) clause assIntv dIntv upperBound lstVarID iVar esl varsList upperBoundLeftValue upperBoundRightValue upperSat false
-(* ======================= END dynamicDecom_new_clause ========================== *)
+open Variable
+open Interval
 
 
-(* This function find an unsat point for decomposition *)
-let rec decompose_find_unsat_point boolExp assIntv dIntv lstVarID ivar esl var lowerBound upperBound currentPoint isFound isIncreasing =
+let add_new_varsIntvsPriority priority addedVarsIntvsMap currentvarsIntvsMapPrioritiesMaps =
+  try
+    let varsIntvsMaps = FloatMap.find priority currentvarsIntvsMapPrioritiesMaps in
+    FloatMap.add priority (addedVarsIntvsMap::varsIntvsMaps) currentvarsIntvsMapPrioritiesMaps
+  with Not_found -> FloatMap.add priority ([addedVarsIntvsMap]) currentvarsIntvsMapPrioritiesMaps
+
+
+let decompose_var varType esl varsIntvsMap polyCons var (intv, varSen, isPositiveSen) varsIntvsMapPrioritiesMaps =
+  (*let (narrowed, newIntv) = polyCons#backward_interval_propagate var intv varsIntvsMiniSATCodesMap in*)
+  let lowerBound = intv.low in
+  let upperBound = intv.high in
   let newPoint = 
-    if isIncreasing then currentPoint +. esl
-    else currentPoint -. esl
-  in 
-  if (isIncreasing && (upperBound -. newPoint) > esl) || ((not isIncreasing) && (newPoint -. lowerBound) > esl) then 
-    let newIntv = 
-      if isIncreasing then new IA.interval lowerBound newPoint
-      else new IA.interval newPoint upperBound
-    in 
-    (*print_endline ("Next Point: " ^ string_of_float newPoint ^ " Lower Bound: " ^ string_of_float lowerBound ^ " " ^ string_of_bool (newPoint > lowerBound));
+    if lowerBound = neg_infinity then 
+      if upperBound = infinity then 0.
+      else upperBound -. 10.
+    else 
+      if upperBound = infinity then lowerBound +. 10. 
+      else 0.5 *. lowerBound +. 0.5 *. upperBound
+  in
+  let unknown = 
+    if varType = "Int" then floor newPoint < lowerBound || ceil newPoint > upperBound
+    else newPoint < lowerBound || newPoint > upperBound
+  in
+  if unknown then
+    add_new_varsIntvsPriority (esl /. 10.) varsIntvsMap varsIntvsMapPrioritiesMaps
+  else
+    let lowerIntv = 
+      if varType = "Int" then
+        let tmpNewPoint = floor newPoint in
+        {low=lowerBound; high=tmpNewPoint}
+      else 
+        {low=lowerBound; high=newPoint}
+    in
+    (* print_endline("lowerIntv" ^ lowerIntv#to_string);
+    flush stdout; *)
+    let upperIntv = 
+      if varType = "Int" then
+        let tmpNewPoint = ceil newPoint in
+        {low=tmpNewPoint;high=upperBound}
+      else 
+        {low=newPoint;high=upperBound}
+    in
+    (* Compute the SAT length of lower interval by IA *)
+    let lowerVarsIntvsMap = StringMap.add var lowerIntv varsIntvsMap in
+    (*print_endline "Start Computing for lower interval";
     flush stdout;*)
-    let (sat,_) = check_sat_af_two_ci boolExp ((var, newIntv)::assIntv) in
-    if sat = -1 then decompose_find_unsat_point boolExp assIntv dIntv lstVarID ivar esl var lowerBound upperBound newPoint true isIncreasing
-    else (isFound, currentPoint)
-  else (isFound, currentPoint)
+    let (lowerSAT, lowerSatLength, lowerEasiness) = polyCons#check_sat_get_satLength lowerVarsIntvsMap in
+    (*print_endline ("Lower: " ^ string_of_int lowerSAT ^ " - " ^ string_of_float lowerSatLength ^ " - easiness: " ^ string_of_float lowerEasiness);
+    flush stdout;*)
+    
+    (* Compute the SAT length of upper interval by IA *)
+    let upperVarsIntvsMap = StringMap.add var upperIntv varsIntvsMap in
+    let (upperSAT, upperSatLength, upperEasiness) = polyCons#check_sat_get_satLength upperVarsIntvsMap in
+    (*print_endline ("Upper: " ^ string_of_int upperSAT ^ " - satLength: " ^ string_of_float upperSatLength ^ " - easiness: " ^ string_of_float upperEasiness);
+    flush stdout;*)
+
+    if lowerSAT = -1 then 
+      if upperSAT = -1 then varsIntvsMapPrioritiesMaps
+      else add_new_varsIntvsPriority esl upperVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if lowerSAT = 1 then add_new_varsIntvsPriority esl lowerVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if upperSAT = -1 then add_new_varsIntvsPriority esl lowerVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if upperSAT = 1 then add_new_varsIntvsPriority esl upperVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if lowerBound = neg_infinity then
+      let varsIntvsMapPrioritiesMaps = add_new_varsIntvsPriority (esl /. 10.) lowerVarsIntvsMap varsIntvsMapPrioritiesMaps in
+      add_new_varsIntvsPriority esl upperVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if upperBound = infinity then 
+      let varsIntvsMapPrioritiesMaps = add_new_varsIntvsPriority (esl /. 10.) upperVarsIntvsMap varsIntvsMapPrioritiesMaps in
+      add_new_varsIntvsPriority esl lowerVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else if lowerEasiness < upperEasiness then 
+      let varsIntvsMapPrioritiesMaps = add_new_varsIntvsPriority esl lowerVarsIntvsMap varsIntvsMapPrioritiesMaps in
+      add_new_varsIntvsPriority esl upperVarsIntvsMap varsIntvsMapPrioritiesMaps
+    else
+      let varsIntvsMapPrioritiesMaps = add_new_varsIntvsPriority esl upperVarsIntvsMap varsIntvsMapPrioritiesMaps in
+      add_new_varsIntvsPriority esl lowerVarsIntvsMap varsIntvsMapPrioritiesMaps
 
 
-(* This functions try to decompose the interval of a specified variable so that one of the decomposed
-   interval is UNSAT *)
-let rec decompose_unsat_detection_for_var boolExp assIntv dIntv lstVarID iVar esl var =
-  let intv = List.assoc var assIntv in
-  let (isFound, decomposedPoint) = decompose_find_unsat_point boolExp assIntv dIntv lstVarID iVar esl var intv#l intv#h intv#l false true in
-  if isFound then (
-    print_endline ("Decomposed lower: " ^ var ^ " " ^ string_of_float intv#l ^ " " ^ string_of_float intv#h ^ ": " ^ string_of_float decomposedPoint);
-    flush stdout;
-    decompose var intv decomposedPoint lstVarID iVar dIntv true
-  )
-  else 
-    let (isFound, decomposedPoint) = decompose_find_unsat_point boolExp assIntv dIntv lstVarID iVar esl var intv#l intv#h intv#h false false in
-    if isFound then (
-      print_endline ("Decomposed upper: " ^ var ^ " " ^ string_of_float intv#l ^ " " ^ string_of_float intv#h ^ ": " ^ string_of_float decomposedPoint);
+(*Binary balance decomposition on intervals*)
+let dynamicDecom varsIntvsMap varsIntvsMapPrioritiesMaps polyCons unkownPolyConstraints maxDecomposedVarsNum esl remainingTime = 
+  print_endline ("Decomposing: " ^ polyCons#to_string_infix ^ ": " ^ string_of_int polyCons#get_miniSATCode);
+  flush stdout;
+  let startTime = Sys.time() in
+  (* let add_varsSet currentVarsSet polyCons = VariablesSet.union polyCons#get_varsSet currentVarsSet in
+  let varsSet = List.fold_left add_varsSet VariablesSet.empty unkownPolyConstraints in *)
+  let varsSet = polyCons#get_varsSet in
+
+  let not_smallIntv intv =
+
+    let lowerBound = intv.low in
+    let upperBound = intv.high in
+    if lowerBound = neg_infinity && upperBound < esl -. max_float then 
+      false
+    else if upperBound = infinity && lowerBound > max_float -. esl then
+      false
+    else if upperBound <= esl +. lowerBound then 
+      false
+    else
+      true
+  in
+  let add_notSmallInterval var (reducedVarsSet, infVar) = 
+    let intv = StringMap.find var varsIntvsMap in
+    if not_smallIntv intv then 
+      let newInfVar = 
+        if intv.high = infinity || intv.low = neg_infinity then var
+        else infVar
+      in
+      (VariablesSet.add var reducedVarsSet, newInfVar)
+    else (
+      print_endline ("small interval: " ^ var ^ ": " ^ sprintf_I "%f" intv);
       flush stdout;
-      decompose var intv decomposedPoint lstVarID iVar dIntv false
+      (reducedVarsSet, infVar)
     )
-    else ("","","",false)
+  in
+  let (reducedVarsSet, infVar) = (*varsSet*) VariablesSet.fold add_notSmallInterval varsSet (VariablesSet.empty, "") in
+  if VariablesSet.is_empty reducedVarsSet then (*Stop decomposition*) 
+    (* let add_learnt_var var learntVars = 
+      let (_, varId) = StringMap.find var varsIntvsMiniSATCodesMap in
+      "-" ^ string_of_int varId ^ " " ^ learntVars
+    in
+    let polysMiniSATCodeString = 
+      if polyCons#get_miniSATCode > 0 then "-" ^ string_of_int polyCons#get_miniSATCode
+      else string_of_int (polyCons#get_miniSATCode)
+    in
+    let learntClauses = VariablesSet.fold add_learnt_var varsSet (polysMiniSATCodeString ^ " 0") in
+    ((miniSATCodesVarsIntvsMap, nextMiniSATCode), learntClauses, "", false) *)
+    add_new_varsIntvsPriority (esl /. 10.) varsIntvsMap varsIntvsMapPrioritiesMaps
+  else (*Continue decomposition*)
+    
+    (*print_endline (string_of_bool polyCons#isInfinite);
+    flush stdout;*)
+    let decomposedVarsList = 
+      if infVar <> "" then [(infVar, 0., 0)]
+      else polyCons#get_n_varsSen_fromSet maxDecomposedVarsNum (*(VariablesSet.cardinal reducedVarsSet)*) reducedVarsSet 
+    in
+    let add_varIntvMiniSATCode currentVarsIntvsMiniSATCodesIsPositiveSenMap (var, varSen, isPositiveSen) = 
+      let intvMiniSATCode = StringMap.find var varsIntvsMap in
+      StringMap.add var (intvMiniSATCode, varSen, isPositiveSen) currentVarsIntvsMiniSATCodesIsPositiveSenMap
+    in
+    let decomposedVarsIntvsMiniSATCodesIsPositiveMap = List.fold_left add_varIntvMiniSATCode StringMap.empty decomposedVarsList in
+    let varType =
+      if polyCons#get_logic = "QF_NIA" then "Int"
+      else "Real"
+    in 
+    StringMap.fold (decompose_var varType esl varsIntvsMap polyCons) decomposedVarsIntvsMiniSATCodesIsPositiveMap varsIntvsMapPrioritiesMaps
 
 
-(* This is the help function for decompose_unsat_detection *)
-let rec decompose_unsat_detection_extra boolExp assIntv dIntv lstVarID iVar esl varsList =
-  match varsList with
-  | [] -> ("","","",false)
-  | var::remainingVars -> (
-    let (newDIntv, minisatCode, bumpVars, wasDecomposed) = decompose_unsat_detection_for_var boolExp assIntv dIntv lstVarID iVar esl var in
-    if wasDecomposed then (newDIntv, minisatCode, bumpVars, wasDecomposed)
-    else decompose_unsat_detection_extra boolExp assIntv dIntv lstVarID iVar esl remainingVars
-  )
+(*Binary balance decomposition on intervals*)
+let dynamicDecom_noStrategy varsIntvsMap varsIntvsMapPrioritiesMaps polyCons unkownPolyConstraints maxDecomposedVarsNum esl remainingTime =
+  print_endline ("Decomposing: " ^ polyCons#to_string_infix ^ ": " ^ string_of_int polyCons#get_miniSATCode);
+  flush stdout;
+  let startTime = Sys.time() in
+  let add_varsSet currentVarsSet polyCons = VariablesSet.union polyCons#get_varsSet currentVarsSet in
+  let varsSet = List.fold_left add_varsSet VariablesSet.empty unkownPolyConstraints in
 
-(* This function tries to extends the length of decomposed intervals so that the api is unsat inside that interval *)
-let decompose_unsat_detection boolExp assIntv dIntv lstVarID iVar esl =
-  (*print_endline (bool_expr_to_infix_string boolExp);
-  flush stdout;*)
-  let varsList = Util.red_list (bool_vars boolExp) in
-  decompose_unsat_detection_extra boolExp assIntv dIntv lstVarID iVar esl varsList
-  
-  
-let rec decompose_list_unsat_detection boolExpList assIntv dIntv lstVarID iVar esl = 
-  match boolExpList with 
-  | [] -> ("","","",false)
-  | boolExp::remainingBoolExps -> 
-    let (newDIntv, minisatCode, bumpVars, wasDecomposed) = decompose_unsat_detection boolExp assIntv dIntv lstVarID iVar esl in
-    if wasDecomposed then (newDIntv, minisatCode, bumpVars, wasDecomposed)
-    else decompose_list_unsat_detection remainingBoolExps assIntv dIntv lstVarID iVar esl *)
+  let (var, intv) = String
 
+  let (reducedVarsSet, infVar) = (*varsSet*) VariablesSet.fold add_notSmallInterval varsSet (VariablesSet.empty, "") in
+  if VariablesSet.is_empty reducedVarsSet then (*Stop decomposition*) 
+    (* let add_learnt_var var learntVars = 
+      let (_, varId) = StringMap.find var varsIntvsMiniSATCodesMap in
+      "-" ^ string_of_int varId ^ " " ^ learntVars
+    in
+    let polysMiniSATCodeString = 
+      if polyCons#get_miniSATCode > 0 then "-" ^ string_of_int polyCons#get_miniSATCode
+      else string_of_int (polyCons#get_miniSATCode)
+    in
+    let learntClauses = VariablesSet.fold add_learnt_var varsSet (polysMiniSATCodeString ^ " 0") in
+    ((miniSATCodesVarsIntvsMap, nextMiniSATCode), learntClauses, "", false) *)
+    add_new_varsIntvsPriority (esl /. 10.) varsIntvsMap varsIntvsMapPrioritiesMaps
+  else (*Continue decomposition*)
+    (*print_endline (string_of_bool polyCons#isInfinite);
+    flush stdout;*)
+    let decomposedVarsList = 
+      if infVar <> "" then [(infVar, 0., 0)]
+      else polyCons#get_n_varsSen_fromSet maxDecomposedVarsNum (*(VariablesSet.cardinal reducedVarsSet)*) reducedVarsSet 
+    in
+    let add_varIntvMiniSATCode currentVarsIntvsMiniSATCodesIsPositiveSenMap (var, varSen, isPositiveSen) = 
+      let intvMiniSATCode = StringMap.find var varsIntvsMap in
+      StringMap.add var (intvMiniSATCode, varSen, isPositiveSen) currentVarsIntvsMiniSATCodesIsPositiveSenMap
+    in
+    let decomposedVarsIntvsMiniSATCodesIsPositiveMap = List.fold_left add_varIntvMiniSATCode StringMap.empty decomposedVarsList in
+    let varType =
+      if polyCons#get_logic = "QF_NIA" then "Int"
+      else "Real"
+    in 
+    StringMap.fold (decompose_var varType esl varsIntvsMap polyCons) decomposedVarsIntvsMiniSATCodesIsPositiveMap varsIntvsMapPrioritiesMaps
