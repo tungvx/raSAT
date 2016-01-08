6876d6c9a29feaf5e6fbd65879152012ccd3fb39
Vu Xuan Tung <toilatung90@gmail.com>
2016-01-08 23:56:45 +0900

    fix test.py

--- solver/Main.cc
+++ solver/Main.cc
@@ -212,7 +212,7 @@ int main(int argc, char* argv[]) {
   //return 0;
 	////cout << "Run" << endl;
 	bool debug = false;
-	bool statistics = false;
+	bool statistics = true;
 	bool smtComp = false;
 	double initial_time = cpuTime();
 	/* Initialize Caml code */
--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import sys
 import concurrent.futures
 import copy
 import re
+from subprocess import TimeoutExpired
+import time
 
 SOLVED_PROBLEM="*.smt2"
 
@@ -32,21 +34,22 @@ RASAT_RESULT='raSATResult'
 EQ='EQ'
 NEQ='NEQ'
 
-HEADERS=[PROBLEM, N_VARS, MAX_VARS, N_APIS, TIME, IA_TIME, TESTING_TIME, US_CORE_TIME
-         PARSING_TIME, DECOMPOSITION_TIME, MINISAT_TIME, MINISAT_VARS, MINISAT_CLAUSES
-         MINISAT_CALLS, RASAT_CLAUSES, DECOMPOSED_LEARNED_CLAUSES, UNKNOWN_LEARNED_CLAUSES
-         RESULT, RASAT_RESULT, EQ, NEQ]
+HEADERS=[PROBLEM, N_VARS, MAX_VARS, N_APIS, TIME, IA_TIME, TESTING_TIME, US_CORE_TIME,
+         PARSING_TIME, DECOMPOSITION_TIME, MINISAT_TIME, MINISAT_VARS, MINISAT_CLAUSES,
+         MINISAT_CALLS, RASAT_CLAUSES, DECOMPOSED_LEARNED_CLAUSES, UNKNOWN_LEARNED_CLAUSES,
+         UNSAT_LEANRED_CLAUSES, RESULT, RASAT_RESULT, EQ, NEQ]
+
 
 default_result = {
-          PROBLEM:os.path.join(root, filename),
+          PROBLEM:"",
           N_VARS: 0,
           MAX_VARS: 0,
           N_APIS: 0,
           TIME: 0,
           IA_TIME: 0,
           TESTING_TIME: 0,
-          US_CORE_TIME 0,
-          PARSING_TIME: parsingTime,
+          US_CORE_TIME: 0,
+          PARSING_TIME: 0,
           DECOMPOSITION_TIME: 0,
           MINISAT_TIME: 0,
           MINISAT_VARS: 0,
@@ -54,10 +57,10 @@ default_result = {
           MINISAT_CALLS: 0,
           RASAT_CLAUSES: 0,
           DECOMPOSED_LEARNED_CLAUSES: 0,
-          UNSAT_LEANRED_CLAUSES 0,
+          UNSAT_LEANRED_CLAUSES: 0,
           UNKNOWN_LEARNED_CLAUSES: 0,
           RESULT: 'unknown',
-          raSATResult: 'unknown', 
+          RASAT_RESULT: 'unknown', 
           EQ: '0',
           NEQ: '0'
 }
@@ -86,29 +89,33 @@ def remove_tmp(root, filename):
 
 def set_result(result, root, filename):
   try:
-    with open(os.path.join(root, filename) + '.tmp', 'rb') as csvfile:
+    with open(os.path.join(root, filename) + '.tmp', 'r') as csvfile:
       reader = csv.reader(csvfile)
-      output = reader.next()
+      output = reader.__next__()
 
       # set numbers
       for index in range(1, 4):
         result[HEADERS[index]] = output[index]
 
       for index in range(4, 18):
+        # print (index, HEADERS[index], output[index])
         result[HEADERS[index]] += float(output[index])
       
 
-      for index in range(18, 21):
-        result[HEADERS[index]] = output[index]
+      result[EQ] = output[18]
+      result[NEQ] = output[19]
+      result[RASAT_RESULT] = output[20]
   except IOError:
     result[RASAT_RESULT] = 'timeout'
 
-def solve(filename):
-  global root, initSbox, initLowerBound, initUpperBound, timeout
+def solve(args):
+  # global root, initSbox, initLowerBound, initUpperBound, timeout
+  (filename,root, initSbox, initLowerBound, initUpperBound, timeout) = args
 
-  remove_tmp(root, filename)
+  startingTime = time.time()
 
   result = copy.deepcopy(default_result)
+  result[PROBLEM] = os.path.join(root, filename)
 
   sbox = initSbox * 10
   lowerBound = initLowerBound
@@ -118,7 +125,7 @@ def solve(filename):
   try:
     f = open(os.path.join(root, filename))
     '(set-info :status sat)'
-    m = re.search('\(set-info :status (sat|unsat|unknown))\)', f.read())
+    m = re.search('\(set-info :status (sat|unsat|unknown)\)', f.read())
     if m:
       result[RESULT]=m.group(1)
   except IOError:
@@ -128,116 +135,43 @@ def solve(filename):
   bounds = ['lb=-10 10', 'lb=-inf inf']
   boundsNum = len(bounds)
   boundIndex = 0
-  while (result[RASAT_RESULT] != 'sat' and result[TIME] < timeout and boundIndex < boundsNum):
+  while (result[RASAT_RESULT] != 'sat' and timeout > 0 and boundIndex < boundsNum):
+    # Remove temp files
+    remove_tmp(root, filename)
+
     try:
-      subprocess.run(["./raSAT", os.path.join(root, filename), bounds[boundIndex]], timeout=timeout- result[TIME])
+      subprocess.call(["./raSAT", os.path.join(root, filename), bounds[boundIndex]], timeout=timeout)
     except TimeoutExpired:
       return result
 
     set_result(result, root, filename)
 
-    if raSATResult != 'sat':
-      boundIndex += 1
-
     # Remove temp files
     remove_tmp(root, filename)
 
-    try:
-      subprocess.Popen(["./raSAT", os.path.join(root, filename), 'lb=' + str(lowerBound) + ' ' + str(upperBound)])
-      proc.wait()
-      signal.alarm(0)
-    except Alarm:
-      proc.kill()
-      raSAT_result = 'timeout'
-      pass
+    boundIndex += 1
 
-    try:
-      with open(os.path.join(root, filename) + '.tmp', 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        output = reader.next()
-        nVars = output[1]
-        maxVars = output[2]
-        nAPIs = output[3]
-        time += float(output[4])
-        iaTime += float(output[5])
-        testingTime += float(output[6])
-        usTime += float(output[7])
-        parsingTime += float(output[8])
-        decompositionTime += float(output[9])
-        miniSATTime += float(output[10])
-        miniSATVars += float(output[11])
-        miniSATClauses += float(output[12])
-        miniSATCalls += float(output[13])
-        raSATClauses += float(output[14])
-        decomposedLearnedClauses += float(output[15])
-        UNSATLearnedClauses += float(output[16])
-        unknownLearnedClauses += float(output[17])
-        isEquation = output[18]
-        isNotEquation = output[19]
-        raSATResult = output[20]
-        csvfile.close()
-    except IOError:
-      raSATResult = 'timeout'
+    timeout = timeout - (time.time() - startingTime)
+
+  return result
 
 
 
-def run(directory, initLowerBound, initUpperBound, initSbox, timeout, resultFile):
-  with concurrent.futures.ProcessPoolExecutor() as executor:
-    for root, dirnames, filenames in os.walk(directory):
-      results = executor.map(solve, fnmatch.filter(filenames, PROBLEM))    
+def run(directory, initLowerBound, initUpperBound, initSbox, timeout, resultFile, PROCESSES_NUM):
+  with concurrent.futures.ProcessPoolExecutor(PROCESSES_NUM) as executor:
+    with open(os.path.join(directory, resultFile), 'w+', 1) as csvfile:
+      spamwriter = csv.DictWriter(csvfile, fieldnames=HEADERS)
+      spamwriter.writeheader()
+      for root, dirnames, filenames in os.walk(directory):
+        results = executor.map(solve, [(filename,root, initSbox, initLowerBound, 
+                                        initUpperBound, timeout) 
+                                      for filename in fnmatch.filter(filenames, SOLVED_PROBLEM)])
+        for result in results:
+          for key in result:
+            result[key] = str(result[key])
+          spamwriter.writerow(result)
 
-  
-  #sbox = initSbox
-  solvedProblems = 0
-  with open(os.path.join(directory, resultFile), 'wb') as csvfile:
-    spamwriter = csv.DictWriter(csvfile, fieldnames=HEADERS)
-    spamwriter.writeheader()
-    spamwriter.writerows(results)
-  for root, dirnames, filenames in os.walk(directory):
-    for filename in fnmatch.filter(filenames, '*.smt2'):
-      
-      
-        
-      
-      
-      if raSATResult == 'sat' or raSATResult == 'unsat':
-        solvedProblems += 1     
-      with open(os.path.join(directory, resultFile), 'a') as csvfile:
-        spamwriter = csv.writer(csvfile)
-        spamwriter.writerow([os.path.join(root, filename), nVars, maxVars, nAPIs, time, iaTime, testingTime, usTime, parsingTime, decompositionTime, miniSATTime, miniSATVars, miniSATClauses, miniSATCalls, raSATClauses, decomposedLearnedClauses, UNSATLearnedClauses, unknownLearnedClauses, result, raSATResult, isEquation, isNotEquation])
-        csvfile.close()
-
-
-      # if (raSATResult == 'timeout'):
-      #   sys.exit()
-
-      # print os.path.join(root, filename), raSATResult
-     
-      try:
-        os.remove(os.path.join(root, filename) + '.tmp')
-      except OSError:
-        pass
-        
-      try:
-        os.remove(os.path.join(root, filename)[:-5] + '.in')
-      except OSError:
-        pass
-     
-      try:
-        os.remove(os.path.join(root, filename)[:-5] + '.out')
-      except OSError:
-        pass
-        
-      try:
-        os.remove(os.path.join(root, filename)[:-5] + '.rs')
-      except OSError:
-        pass
-  with open(os.path.join(directory, resultFile), 'a') as csvfile:
-    spamwriter = csv.writer(csvfile)
-    spamwriter.writerow(['Problem', 'nVars', 'maxVars', 'nAPIs', 'time', 'iaTime', 'testingTime', 'usCoreTime', 'parsingTime', 'decompositionTime', 'miniSATTime', 'miniSATVars', 'miniSATClauses', 'miniSATCalls', 'raSATClauses', 'decomposedLearnedClauses', 'UNSATLearnedClauses', 'unknownLearnedClauses', 'result', solvedProblems, 'EQ', 'NEQ'])
-    csvfile.close()
-
-run ('Test/smtlib-20140121/QF_NRA/zankl', -10, 10, 0.1, 10, '1-5-8-11_zankl.csv')
+# run ('Test/smtlib-20140121/QF_NRA/zankl/', -10, 10, 0.1, 10, '1-5-8-11.csv', 2)
 #run ('zankl', -10, 10, 0.1, 500, 'with_dependency_sensitivity_restartSmallerBox_boxSelectionUsingSensitivity.xls')
 #run ('QF_NRA/meti-tarski', -10, 10, 0.1, 500, 'with_dependency_sensitivity_restartSmallerBox_boxSelectionUsingSensitivity.xls')
 #run ('Test/meti-tarski', -1, 1, 0.1, 60, 'result.xls')
